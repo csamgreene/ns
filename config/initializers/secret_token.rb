8a7aa32375689df8670d388b5312a63f3e316582
U-LUXRESEARCHINC\chris.greene <chris.greene@lux-bos-e6320-6.luxresearchinc.com>
2011-09-22 15:14:35 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ns::Application.config.secret_token = 'ab0930ec15b97edac79f4009000aef0d3846294577f8d62bc752b9af6341f20489addf3c364627ddb2900df7c9bd6fcf52faa4d21ad5daa1d176e4f35738b2c7'
